3d50bfc761d45b9fa5b44830e14ec0bcb75d48b2
Harsha <cs20btech11028@iith.ac.in>
2021-07-13 21:02:33 +0530

    Add collision,types

--- Project/Tile.cpp
+++ Project/Tile.cpp
@@ -1,31 +1,60 @@
 #include "Tile.h"
 
-Tile::Tile(float x,float y,float gridsize,sf::Texture& texture,sf::IntRect& texturerect)
+Tile::Tile(float x,float y,float gridsize,sf::Texture& texture,sf::IntRect& texturerect,int type,bool collision)
 {
-	tile.setSize(sf::Vector2f(gridsize,gridsize));
-	tile.setPosition(x,y);
-	tile.setFillColor(sf::Color::White);
-	tile.setTexture(&texture);
-	tile.setTextureRect(texturerect);
+	//tile.setSize(sf::Vector2f(gridsize,gridsize));
+	tilesprite.setPosition(x,y);
+	tilesprite.setColor(sf::Color::White);
+	tilesprite.setTexture(texture);
+	tilesprite.setTextureRect(texturerect);
+
+	this->gridsize=gridsize;
+	this->texturerect=texturerect;
+	this->type=type;
+	this->collision=collision;
+
+	boundrect = new Boundrect(tilesprite,sf::Vector2f(0,0),gridsize,gridsize);
 }
 Tile::Tile()
 {
-
+	type=0;
+	collision=false;
 }
 
 Tile::~Tile()
 {
 
 }
+
+collider Tile::getcollider()
+{
+	return collider(*boundrect);
+}
+std::ostream& operator<<(std::ostream& os,Tile t)
+{
+	os<< t.tilesprite.getPosition().x <<" "<<t.tilesprite.getPosition().y<<" "<<t.texturerect.left<<" "<<t.texturerect.top<<\
+	" "<<t.type <<" "<< t.collision;
+
+	return os;
+}
+bool Tile::getcollision()
+{
+	return collision;
+}
 void Tile::update()
 {
-
+	if(boundrect)
+	boundrect->update();
 }
 
 void Tile::render(sf::RenderTarget* target)
 {
 	if(target)
 	{
-		target->draw(tile);
+		target->draw(tilesprite);
+	}
+	if(boundrect)
+	{
+		boundrect->render(target);
 	}
 }
--- Project/Tile.h
+++ Project/Tile.h
@@ -1,14 +1,26 @@
 #ifndef TILE_H
 #define TILE_H
 #include <SFML/Graphics.hpp>
+#include "collider.h"
+#include "Bound_Rect.h"
 class Tile
 {
 private:
-	sf::RectangleShape tile;
+	
+	sf::Sprite tilesprite;
+	bool collision;
+	Boundrect* boundrect;
+	float gridsize;
+	sf::IntRect texturerect;
+	int type;
 public:
 	Tile();
-	Tile(float x,float y,float gridsize,sf::Texture& texture,sf::IntRect& texturerect);
+	Tile(float x,float y,float gridsize,sf::Texture& texture,sf::IntRect& texturerect,int type,bool collison);
 	~Tile();
+
+	friend std::ostream& operator<<(std::ostream& out,Tile);
+	collider getcollider();
+	bool getcollision();
 	void update();
 	void render(sf::RenderTarget* target);
 };
