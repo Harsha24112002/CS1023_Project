d187aa0d26e542c1e5c265ebe0435309fce51d71
Harsha <cs20btech11028@iith.ac.in>
2021-07-13 21:02:58 +0530

    Add save,load

--- Project/Tilemap.cpp
+++ Project/Tilemap.cpp
@@ -1,12 +1,13 @@
 #include "Tilemap.h"
-
-Tilemap::Tilemap(float gridsize,float width,float height)
+#include <fstream>
+Tilemap::Tilemap(float gridsize,unsigned width,unsigned height)
 {
 	this->gridsizeu=(unsigned)gridsize;
 	this->gridsizef =gridsize;
 	this->mapsize.x=width;
 	this->mapsize.y=height;
 	this->layers=1;
+//	std::cout<<mapsize.x<<mapsize.y<<std::endl;
 	this->texture.loadFromFile("Resources/tiles_2.png");
 	for(unsigned x=0;x<mapsize.x;x++)
 	{
@@ -29,15 +30,27 @@ Tilemap::~Tilemap()
 } 
 void Tilemap::update()
 {
-	
+	for(auto& a:map)
+	{
+		for(auto& b : a)
+		{
+			for(auto& c:b)
+			{
+				if(c!=nullptr)
+				{
+					c->update();
+				}
+			}		
+		}
+	}
 }
-void Tilemap::addtile(unsigned x,unsigned y ,unsigned z,sf::IntRect& texturerect)
+void Tilemap::addtile(unsigned x,unsigned y ,unsigned z,sf::IntRect& texturerect,int type,bool collison)
 {
 	if(x<mapsize.x && x>=0 &&y<mapsize.y && y>=0)
 	{
 	if(map[x][y][z]==nullptr)
 	{
-		map[x][y][z]=(new Tile(x*gridsizef,y*gridsizef,gridsizef,texture,texturerect));
+		map[x][y][z]=(new Tile(x*gridsizef,y*gridsizef,gridsizef,texture,texturerect,type,collison));
 	}
 	}
 }
@@ -49,6 +62,86 @@ void Tilemap::removetile(unsigned x,unsigned y ,unsigned z)
 		map[x][y][z]=nullptr;
 	}
 }
+
+void Tilemap::checkcollison(Player* player,sf::Vector2f& direction)
+{
+	collider collider=player->getcollider();
+	for(auto& a:map)
+	{
+		for(auto& b: a)
+		{
+			for(auto& c:b)
+			{
+				if(c!=nullptr && c->getcollision())
+				{
+					if(c->getcollider().checkcollision(collider,direction,1.0f))
+					{
+						player->oncollision(direction);
+					}
+				}
+			}
+		}
+	}
+}
+
+void Tilemap::savetofile(std::string& filename)
+{
+	std::ofstream savefile;
+	savefile.open(filename);
+	if(savefile.is_open())
+	{
+		savefile<<mapsize.x<<" "<<mapsize.y<<"\n"<<gridsizef<<"\n"<<layers<<"\n";
+		for(auto& a:map)
+		{
+			for(auto& b:a)
+			{
+				for(auto& c:b)
+				{
+					if(c!=nullptr)
+					{
+					savefile<<*c<<" ";
+					}
+				}
+			}
+		}
+	}
+	savefile.close();
+}
+
+void Tilemap::Loadfromfile(std::string& filename)
+{
+	std::ifstream loadfile;
+	loadfile.open(filename);
+	if(loadfile.is_open())
+	{
+		if(loadfile>>mapsize.x>>mapsize.y)
+		{
+
+		}
+		if(loadfile>>gridsizef>>layers)
+		{
+
+		}
+		sf::Vector2f positions;
+		sf::IntRect texrect;
+		int type;
+		bool collison;
+		while(loadfile>>positions.x>>positions.y>>texrect.left>>texrect.top>>type>>collison)
+		{
+			int a=positions.x/gridsizef;
+			int b=positions.y/gridsizef;
+			texrect.width=(float)gridsizef;
+			texrect.height=(float)gridsizef;
+			if(map[a][b][0]==nullptr)
+			{
+			map[a][b][0]= new Tile(positions.x,positions.y,gridsizef,texture,texrect,type,collison);
+			}
+		}
+
+	}
+	loadfile.close();			
+}
+
 void Tilemap::render(sf::RenderTarget* target)
 {
 	for(auto& a:this->map)
--- Project/Tilemap.h
+++ Project/Tilemap.h
@@ -2,7 +2,7 @@
 #define TILEMAP_H
 
 #include "Tile.h"
-
+#include "Player.h"
 class Tilemap
 {
 private:
@@ -14,13 +14,16 @@ private:
 	sf::Texture texture;
 
 public:
-	Tilemap(float gridsize,float width,float height);
+	Tilemap(float gridsize,unsigned width,unsigned height);
 	~Tilemap();
 	void update();
 	sf::Texture* getTilesheet(){return &texture;}
 	void render(sf::RenderTarget* target);
-	void addtile(unsigned x,unsigned y,unsigned z,sf::IntRect& texturerect);
+	void addtile(unsigned x,unsigned y,unsigned z,sf::IntRect& texturerect,int type,bool collision);
 	void removetile(unsigned x,unsigned y ,unsigned z);
+	void checkcollison(Player* player,sf::Vector2f& direction);
+	void savetofile(std::string& filename);
+	void Loadfromfile(std::string& filename);
 };
 
 #endif
